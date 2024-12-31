1968a9e9c7206093d1af0b4cca9a5bbc0291b632
Saurav Singh <sauravarya952362@gmail.com>
2024-12-31 16:14:17 +0530

    updation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello React</h1>
+      <h2>React is a JavaScript library for building user interfaces.React is used to build single-page applications.</h2>
+      <p>Jordan Walke, a software engineer at facebook, created React.</p>
     </div>
   )
 }
